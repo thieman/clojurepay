b4006b21f7cc8606c19ead4398a24fde6cf20974
tthieman <travis.thieman@gmail.com>
2013-10-01 18:49:22 -0400

    Where we're going, we don't need SQL.

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://clojurepay.travisthieman.com"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.5"]
-                 [clj-http "0.7.7"]]
+                 [clj-http "0.7.7"]
+                 [com.novemberain/monger "1.5.0"]]
   :plugins [[lein-ring "0.8.5"]]
   :ring {:handler clojurepay.handler/app}
   :profiles
