8767370b3d939b9ff9d3474f6a896f96a9e7f924
tthieman <travis.thieman@gmail.com>
2013-10-11 13:08:52 -0400

    Refactor the API to use models for DB ops

--- src/clojurepay/api.clj
+++ src/clojurepay/api.clj
@@ -41,46 +41,40 @@
 
 (defmethod circle :delete [method id]
   (with-args [id]
-    (let [circle-doc (mc/find-map-by-id "circle" (ObjectId. id))]
-      (if-not (is-member? circle-doc)
+    (let [circle (fetch (->User) id)]
+      (if-not (is-member? circle)
         {:status 401}
-        (do (mc/remove "circle" {:_id (ObjectId. id)})
+        (do (delete circle)
             {:body {} :status 200})))))
 
 (defn circle-reassign-owner [circle-id user-id]
-  (let [circle-doc (mc/find-map-by-id "circle" (ObjectId. circle-id))
-        user-is-owner? (fn [user] (= (:id user) (get-in circle-doc [:owner :id])))
-        new-owner-id (->> (:users circle-doc)
+  (let [circle (fetch (->Circle) circle-id)
+        user-is-owner? (fn [user] (= (:id user) (get-in circle [:owner :id])))
+        new-owner-id (->> (:users circle)
                           (remove user-is-owner?)
                           (first)
                           (:id))
-        new-owner-doc (mc/find-map-by-id "user" new-owner-id)]
-    (if-not (owns-circle? circle-doc)
+        new-owner (fetch (->User) new-owner-id)]
+    (if-not (owns-circle? circle)
       {:status 401}
-      (do (mc/update-by-id "circle"
-                           (ObjectId. circle-id)
-                           {$set {:owner {:id new-owner-id
-                                          :name (:name new-owner-doc)}}})
+      (do (update circle {$set {:owner {:id new-owner-id
+                                        :name (:name new-owner)}}})
           {:body {} :status 200}))))
 
 (defn circle-remove-member [circle-id user-id]
-  (let [circle-doc (mc/find-map-by-id "circle" (ObjectId. circle-id))]
-    (if-not (or (owns-circle? circle-doc) (= user-id (session-get :user)))
+  (let [circle (fetch (->Circle) circle-id)]
+    (if-not (or (owns-circle? circle) (= user-id (session-get :user)))
       {:status 401}
-      (do (mc/update-by-id "circle"
-                           (ObjectId. circle-id)
-                           {$pull {:users {:id user-id}}})
+      (do (update circle {$pull {:users {:id user-id}}})
           {:body {} :status 200}))))
 
 (defn circle-join [circle-id invite-code]
-  (let [circle-doc (mc/find-map-by-id "circle" (ObjectId. circle-id))
-        is-already-member (some #{(session-get :user)} (map :id (:users circle-doc)))
-        valid (and (not (nil? circle-doc))
+  (let [circle (fetch (->Circle) circle-id)
+        is-already-member (some #{(session-get :user)} (map :id (:users circle)))
+        valid (and (not (nil? circle))
                    (not is-already-member)
-                   (= invite-code (:invite_code circle-doc)))]
+                   (= invite-code (:invite_code circle)))]
     (if-not valid
       {:status 400}
-      (do (mc/update-by-id "circle"
-                           (ObjectId. circle-id)
-                           {$push {:users {:id (session-get :user)}}})
+      (do (update circle {$push {:users {:id (session-get :user)}}})
           {:status 200}))))
--- src/clojurepay/model.clj
+++ src/clojurepay/model.clj
@@ -9,62 +9,78 @@
   (fetch-collection [_ record-type query sort]))
 
 (defprotocol Document
-  (opts [_ k])
-  (fetch [_ id])
-  (parse [_ doc]))
+  (opts [this k])
+  (fetch [this id])
+  (delete [this])
+  (parse [this doc]))
 
 (defprotocol PersistableCircle
-  (insert [_ name owner-doc])
-  (update [_]))
+  (insert [this name owner-doc])
+  (update [this args]))
 
 (defrecord User []
+
   Document
-  (opts [_ k]
+  (opts [this k]
     (let [config {:coll "user"}]
       (get config k)))
 
-  (fetch [_ _id]
-    (let [user-doc (mc/find-map-by-id "user" (ObjectId. (str _id)))
+  (fetch [this _id]
+    (let [user-doc (mc/find-map-by-id (opts this :coll) (ObjectId. (str _id)))
           new-user (assoc (->User) {:_id _id})]
       (merge new-user user-doc)))
 
-  (parse [_ doc] (merge (->User) doc)))
+  (delete [this]
+    (let [id (:_id this)]
+      (when id (mc/remove (opts this :coll) {:_id (ObjectId. (str id))}))))
+
+  (parse [this doc] (merge (->User) doc)))
 
 (defrecord Circle []
+
   Document
-  (opts [_ k]
+  (opts [this k]
     (let [config {:coll "circle"}]
       (get config k)))
 
-  (fetch [_ _id]
-    (let [circle-doc (mc/find-map-by-id "circle" (ObjectId. (str _id)))
+  (fetch [this _id]
+    (let [circle-doc (mc/find-map-by-id (opts this :coll) (ObjectId. (str _id)))
           new-circle (assoc (->Circle) {:_id _id})]
       (merge new-circle circle-doc)))
 
-  (parse [_ doc] (merge (->Circle) doc))
+  (delete [this]
+    (let [id (:_id this)]
+      (when id (mc/remove (opts this :coll) {:_id (ObjectId. (str id))}))))
+
+  (parse [this doc] (merge (->Circle) doc))
 
   PersistableCircle
-  (insert [_ name owner-doc]
+  (insert [this name owner-doc]
     (let [circle-id (ObjectId. )
           invite-code (random-string 20)
           now (time/now)]
-      (mc/insert "circle" {:_id circle-id
-                           :name name
-                           :owner (-> owner-doc
-                                      (select-keys [:_id :name])
-                                      (swap-keys :_id :id))
-                           :users [(-> owner-doc
-                                       (select-keys [:_id])
-                                       (swap-keys :_id :id))]
-                           :invite_code invite-code
-                           :created now
-                           :updated now})))
+      (mc/insert (opts this :coll)
+                 {:_id circle-id
+                  :name name
+                  :owner (-> owner-doc
+                             (select-keys [:_id :name])
+                             (swap-keys :_id :id))
+                  :users [(-> owner-doc
+                              (select-keys [:_id])
+                              (swap-keys :_id :id))]
+                  :invite_code invite-code
+                  :created now
+                  :updated now})))
 
-  (update [_] (println "update")))
+  (update [this query]
+    (mc/update-by-id (this opts :coll)
+                     (:_id this)
+                     query)
+    (fetch (->User) (:_id this))))
 
 (defrecord RecordCollection []
   PersistableRecordCollection
-  (fetch-collection [_ base-record query sort]
+  (fetch-collection [this base-record query sort]
     (let [coll (opts base-record :coll)
           docs (mq/with-collection coll (mq/find query) (mq/sort sort))]
       (map #(parse base-record %) docs))))
